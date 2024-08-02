ab064e085f3dc63356f8cd1b09b5a02d8f092bc1
zeyadusf <ziayd.usf@gmail.com>
2024-08-02 18:04:53 +0300

    change class name

--- ch02-Working with Text Data/Processed.py
+++ ch02-Working with Text Data/Processed.py
@@ -48,7 +48,7 @@ def read_txt(path: str) -> str:
 
 if __name__ == "__main__":
     path_txt = r'E:\Courses\LLMs\LLMs-from-Scratch\ch02-Working with Text Data\data\the-verdict.txt'
-    text=read_txt(path_txt)
+    text = read_txt(path_txt)
     tokenize_txt = Processed(text, remove_whitespace=True)
     print(tokenize_txt[:30])
     print(tokenize_txt.count('--'))
