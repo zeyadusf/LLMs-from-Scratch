7885f169916d1363074e54f6b19743cf16d0c3ae
zeyadusf <ziayd.usf@gmail.com>
2024-08-22 20:07:58 +0300

    FeedForwardGELU

--- 05-Pretraining on Unlabeled Data/TransformerBlocks.py
+++ 05-Pretraining on Unlabeled Data/TransformerBlocks.py
@@ -9,3 +9,14 @@ class GELU(torch.nn.Module):
         return 0.5 * x * (1 + torch.tanh(
             torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
 
+class FeedForwardGELU(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg['emb_dim'], 4*cfg['emb_dim']),
+            GELU(),
+            nn.Linear(4*cfg['emb_dim'], cfg['emb_dim']),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
