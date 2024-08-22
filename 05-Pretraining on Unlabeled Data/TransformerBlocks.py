4e79b7b438dc5a6f0098bebb935b2e74a10aa8b0
zeyadusf <ziayd.usf@gmail.com>
2024-08-22 20:06:23 +0300

    Add Class GELU

---
+++ 05-Pretraining on Unlabeled Data/TransformerBlocks.py
@@ -0,0 +1,11 @@
+import torch 
+from torch import nn
+
+class GELU(torch.nn.Module):
+    """Gaussian Error Linear Unit"""
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
+
