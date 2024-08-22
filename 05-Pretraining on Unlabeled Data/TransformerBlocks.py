ca165e9710bd602f77a22df41e2d7c58acbd7cd6
zeyadusf <ziayd.usf@gmail.com>
2024-08-22 20:10:13 +0300

    LayerNorm

--- 05-Pretraining on Unlabeled Data/TransformerBlocks.py
+++ 05-Pretraining on Unlabeled Data/TransformerBlocks.py
@@ -40,3 +40,18 @@ class FeedForwardGELU(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True)
+        norm_x = (x - mean) / torch.sqrt(var+self.eps)
+        return self.scale * norm_x + self.shift
+
