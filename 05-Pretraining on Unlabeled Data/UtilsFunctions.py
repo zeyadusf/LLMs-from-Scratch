5e7818ab56e5bf53ee221b7d420d5f1bb7a55f30
zeyadusf <ziayd.usf@gmail.com>
2024-08-23 16:47:58 +0300

    ids to text function

--- 05-Pretraining on Unlabeled Data/UtilsFunctions.py
+++ 05-Pretraining on Unlabeled Data/UtilsFunctions.py
@@ -22,3 +22,7 @@ def text_to_token_ids(text, tokenizer):
     encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
     encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
     return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0) # remove batch dimension
+    return tokenizer.decode(flat.tolist())
